98fc855a59c02d8415bc98d83cbae3244c21ca05
@mkuritsyna <mkuritsyna@ozon.ru>
2024-02-27 17:51:30 +0100

    main.go

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,7 +1,36 @@
-package app
+package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+func hello() {
+	fmt.Println("Привет! Я генератор qr кодов. Введи ссылку, для которой ты хочешь сгенерировать qr-код и я отправлю тебе изображение с кодом.")
+	fmt.Println("Пожалуйста, введи ссылку: ")
+}
+
+func scan(link *string) {
+	fmt.Scan(link)
+}
+
+func validate(link string) bool {
+	if strings.Contains(link, " ") ||
+		strings.HasPrefix(link, "https://") ||
+		strings.HasPrefix(link, "http://") ||
+		strings.HasPrefix(link, "www.") {
+		return true
+	}
+	return false
+}
 
 func main() {
-	fmt.Println("Hello!")
+	var link string
+	hello()
+	scan(&link)
+	if validate(link) {
+		fmt.Println("true")
+	} else {
+		fmt.Println("false")
+	}
 }
